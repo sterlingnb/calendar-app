ed85b38da8b17c26fb2499dfd124f9cf899d7cb6
Sterling Rose <nickbehling2@gmail.com>
2022-10-04 18:29:16 -0500

    tried to enact moment js

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+var today = moment();
+var todayFormatted = today.format("MMM Do, YYYY")
+var gradDate = moment("2021-06-15").format("MMM Do, YYYY");
+$("#1a").text(gradDate);
+
+console.log("Fred")
+<script src="moment.js"></script>
+<script>
+    moment().format();
+</script>
